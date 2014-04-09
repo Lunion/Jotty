d7d801c7aa7f6bbbbbf85a8fa2e9076cb4d1a196
enlerklaw <lawenlerk@gmail.com>
2014-04-09 19:45:17 +0800

    WIP

---
+++ Jotty/src/main/java/com/lawenlerk/jotty/holographlibrary/LineGraphCursorAdapter.java
@@ -0,0 +1,10 @@
+package com.lawenlerk.jotty.holographlibrary;
+
+/**
+ * Created by enlerklaw on 09/04/2014 in project Jotty.
+ */
+public class LineGraphCursorAdapter {
+    // LineGraphCursorAdapter takes in a cursor and converts it into a line for LineGraphView to utilise
+    // It also observes changes in the cursor and will notify LineGraphView accordingly
+
+}
